7ec826a7dda31b54f7ae09c043c9d88557a21780
Alli Sodeeq <sodeeqalli@gmail.com>
2024-07-08 08:21:24 +0100

    Changes made

--- loops.js
+++ loops.js
@@ -1,3 +1,25 @@
 for (let i = 0; i<=10; i++){
     console.log("Hey, how are you!" + i)
 }
+
+const users = ['Max', 'Anna', 'Joel'];
+
+for (const user of users){
+    console.log(user);
+}
+
+const LoggedInUser = {
+    name: 'Max',
+    age: 32,
+    isAdmin: true
+}
+
+for (const keys in LoggedInUser){
+    console.log(LoggedInUser[keys]);
+}
+
+let isFinished = false
+
+while(!isFinished){
+  isFinished =   confirm('Do you want to quit')
+}
